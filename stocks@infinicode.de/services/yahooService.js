2fa50c963005d52f0c0a58fa903651c02ebe5e0a
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2022-05-09 23:17:43 +0200

    Update yahooService.js

--- stocks@infinicode.de/services/yahooService.js
+++ stocks@infinicode.de/services/yahooService.js
@@ -19,7 +19,90 @@ const defaultQueryParameters = {
   corsDomain: 'finance.yahoo.com'
 }
 
+/*************************************************/
+
+const TAX = 0.19;
+const CURR_EXCHANGE_FEE = 0.005;
+const MY_SYMBOL = 'NOD.OL';
+const NOK_TO_USD_SYMBOL = 'NOKUSD=X';
+const USD_TO_PLN_SYMBOL = 'PLN=X';
+const CURRENCY_FIELDS = {
+	summaryDetail: [
+		'previousClose', 'open', 'dayLow', 'dayHigh', 'regularMarketPreviousClose', 'regularMarketOpen',
+		'regularMarketDayLow', 'regularMarketDayHigh', 'bid', 'ask', 'fiftyTwoWeekLow', 'fiftyTwoWeekHigh',
+		'fiftyDayAverage', 'twoHundredDayAverage'
+	],
+	price: [
+		'regularMarketPrice', 'regularMarketDayHigh', 'regularMarketDayLow',
+		'regularMarketPreviousClose', 'regularMarketOpen',
+	]
+};
+
+
+var getMySummary = async (symbol) => {
+
+	let params;
+
+	const COUNT = parseInt(symbol.substring(11));
+
+	try {
+		const queryParameters = {
+			...defaultQueryParameters,
+			modules: 'price%2CsummaryDetail%2CpageViews'
+		}
+
+		let response = await fetch({ url: `${API_ENDPOINT}/${API_VERSION_SUMMARY}/quoteSummary/${MY_SYMBOL}`, queryParameters });
+		let dataStock = response.json();
+		if (!response.ok) throw Error(`${response.statusText} - ${response.text()}`);
+
+		response = await fetch({ url: `${API_ENDPOINT}/${API_VERSION_SUMMARY}/quoteSummary/${NOK_TO_USD_SYMBOL}`, queryParameters });
+		let dataNOK2USD = response.json();
+		if (!response.ok) throw Error(`${response.statusText} - ${response.text()}`);
+
+		response = await fetch({ url: `${API_ENDPOINT}/${API_VERSION_SUMMARY}/quoteSummary/${USD_TO_PLN_SYMBOL}`, queryParameters });
+		let dataUSD2PLN = response.json();
+		if (!response.ok) throw Error(`${response.statusText} - ${response.text()}`);
+
+		const totalMul = (1 - CURR_EXCHANGE_FEE) * (1 - CURR_EXCHANGE_FEE) * (1 - TAX) * COUNT;
+		const defaultRate = dataNOK2USD.quoteSummary.result[0].price.regularMarketPrice * dataUSD2PLN.quoteSummary.result[0].price.regularMarketPrice * totalMul;
+
+		for (let mod in CURRENCY_FIELDS) {
+			for (let field of CURRENCY_FIELDS[mod]) {
+				if ((field in dataNOK2USD.quoteSummary.result[0][mod]) && (field in dataUSD2PLN.quoteSummary.result[0][mod])) {
+					dataStock.quoteSummary.result[0][mod][field] *= dataNOK2USD.quoteSummary.result[0][mod][field] * dataUSD2PLN.quoteSummary.result[0][mod][field] * totalMul;
+				} else {
+					dataStock.quoteSummary.result[0][mod][field] *= defaultRate;
+				}
+			}
+		}
+		dataStock.quoteSummary.result[0].price.regularMarketChange = dataStock.quoteSummary.result[0].price.regularMarketPrice - dataStock.quoteSummary.result[0].price.regularMarketPreviousClose;
+		dataStock.quoteSummary.result[0].price.regularMarketChangePercent = dataStock.quoteSummary.result[0].price.regularMarketChange / dataStock.quoteSummary.result[0].price.regularMarketPreviousClose;
+		dataStock.quoteSummary.result[0].price.currency = 'PLN';
+		dataStock.quoteSummary.result[0].price.currencySymbol = 'zł';
+		dataStock.quoteSummary.result[0].summaryDetail.currency = 'PLN';
+
+		params = {
+			symbol,
+			quoteData: dataStock
+		}
+
+	} catch (ex) {
+		params = {
+			symbol,
+			quoteData: {},
+			error: ex.toString()
+		}
+	}
+
+	return createQuoteSummaryFromYahooData(params);
+}
+
+/*************************************************/
+
 var getQuoteSummary = async ({ symbol }) => {
+  /*************************************************/
+  if (symbol.startsWith('MY.SUMMARY.')) return await getMySummary(symbol);
+  /*************************************************/
   const queryParameters = {
     ...defaultQueryParameters,
     modules: 'price%2CsummaryDetail%2CpageViews'
@@ -42,6 +125,9 @@ var getQuoteSummary = async ({ symbol }) => {
 }
 
 var getHistoricalQuotes = async ({ symbol, range = '1mo', includeTimestamps = true }) => {
+  /*************************************************/
+  if (symbol.startsWith('MY.SUMMARY.')) symbol = MY_SYMBOL;
+  /*************************************************/
   const queryParameters = {
     ...defaultQueryParameters,
     range,
